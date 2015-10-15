590f28b3bfa47d9573d8b3c1c6bdec8c5bc16d07
Lucas <lucas@agtlucas.com>
2015-10-15 20:08:30 -0300

    Adding index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var shel = require('shell');
+var menu = require('menu');
+
+require('electron-debug')();
+require('crash-reporter').start();
+
+let mainWindow;
+
+function createMainWindow() {
+  const win = new BrowserWindow({
+    'title': app.getName(),
+    'show': false,
+    'width': 800,
+    'height': 600,
+    'min-width': 400,
+    'min-height': 200,
+    'title-bar-style': 'hidden-inset',
+    'web-preferences': {
+      'node-integration': false,
+      'web-security': false,
+      'plugins': true
+    }
+  });
+
+  win.loadUrl('https://asana.com/#login');
+  win.on('closed', app.quit);
+  win.on('page-title-updated', (e, title) => updateBadge(title));
+
+  return win;
+}
+
+app.on('ready', () => {
+  mainWindow = createMainWindow();
+
+  const page = mainWindow.webContents;
+
+  page.on('dom-ready', () => {
+    mainWindow.show();
+  });
+
+  page.on('new-window', (e, url) => {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
+});
