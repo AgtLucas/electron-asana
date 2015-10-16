03a459dbe29e8bd4712393715578ce063aa4f1c0
Lucas <lucas@agtlucas.com>
2015-10-15 21:03:26 -0300

    Updates

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function createMainWindow() {
     'height': 600,
     'min-width': 400,
     'min-height': 200,
-    'icon': path.join(__dirname, 'media', 'icon.png'),
+    'icon': path.join(__dirname, 'media', 'Icon.png'),
     'web-preferences': {
       'node-integration': false,
       'web-security': false,
